addbecee932f168d199bda4e8d12a0adc3cb28a3
rasagnavadde <rasagnavadde@gmail.com>
2023-05-16 17:47:41 -0500

    java8tutorial

---
+++ Spring-boot-tutorial/java8-features/src/com/javatechie/Exceptionhandling/CheckedExceptionHandlerConsumer.java
@@ -0,0 +1,9 @@
+package com.javatechie.Exceptionhandling;
+
+
+
+@FunctionalInterface
+public interface CheckedExceptionHandlerConsumer<Target,ExObj extends Exception> {
+
+    public void accept(Target target) throws ExObj;
+}
